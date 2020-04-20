15a2094d2c19f48b13f1e0ce32bbf5f1dd56eb9d
Stephen Muinda <stephenmuinda@Stephens-MacBook-Pro.local>
2020-04-19 21:01:27 -0600

    Added wind direction to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) =>{
         }else if(body.error){
             callback('Unable to find location',undefined)
         }else {
-            callback (undefined, 'It is '+body.current.weather_descriptions[0] + '. It is currently ' +body.current.temperature  +' degrees out. It feels like '+ body.current.feelslike + ' degrees out.')
+            callback (undefined, 'It is '+body.current.weather_descriptions[0] + '. It is currently ' +body.current.temperature  +' degrees out. It feels like '+ body.current.feelslike + ' degrees out. And the wind direction is '+body.current.wind_dir)
             
             }
 
